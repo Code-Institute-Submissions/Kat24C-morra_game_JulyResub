fd32701ac296e9c8ae403407197a0f2f1aee6aa0
Kat24C <kcsurvey47@gmail.com>
2022-05-30 14:58:20 +0000

    Add function game_choice

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
 import random
+
+
+def game_choice():
+    """
+    Here we are going to ask the user to input a number
+    between 1 and 3.
+    The computer will randomize a number between 1 and 3.
+    Then we will compare the two numbers to see if they
+    are the same or different.
+    """
