1ab497513eaa3cd494e7b06a03b28ab0d66fc5fa
Kat24C <kcsurvey47@gmail.com>
2022-05-30 15:06:04 +0000

    Add try and except statement to get users input number and randomize computer number

--- run.py
+++ run.py
@@ -9,3 +9,21 @@ def game_choice():
     Then we will compare the two numbers to see if they
     are the same or different.
     """
+    while True: 
+        try: 
+            user_choice = int(input("Choose a number between 1 and 3: "))
+        except ValueError: 
+            print("Please enter a valid number")
+            user_choice = int(input("Choose a number between 1 and 3: "))
+        
+        computer_guess = random.randint(1, 3)
+        print(f"The computer chose: {computer_guess}\n")
+        
+        try:
+            user_guess = int(input("Guess the computers number 1, 2 or 3: "))
+        except ValueError: 
+            print("Please enter a valid number")
+            user_guess = int(input("Guess the computers number 1, 2, 3: "))
+        
+        computer_choice = random.randint(1, 3)
+        print(f"The computer chose: {computer_choice}\n")
