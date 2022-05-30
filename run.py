77218cbdbe4a538c1171a20f7b79d847fabd7ac9
Kat24C <kcsurvey47@gmail.com>
2022-05-30 15:07:56 +0000

    Add if/else statement to compare the user number and computer number

--- run.py
+++ run.py
@@ -9,6 +9,8 @@ def game_choice():
     Then we will compare the two numbers to see if they
     are the same or different.
     """
+    player_score = 0
+    comp_score = 0
     while True: 
         try: 
             user_choice = int(input("Choose a number between 1 and 3: "))
@@ -27,3 +29,21 @@ def game_choice():
         
         computer_choice = random.randint(1, 3)
         print(f"The computer chose: {computer_choice}\n")
+
+        if user_guess == computer_choice:
+            user_score = user_guess + computer_choice
+            print(f"Well done, You got {user_score}")
+            player_score = player_score + user_score
+            print(f"Your total is {player_score}\n")
+        else:
+            print("Oh dear, try again you got 0")
+            print(f"Your total is {player_score}")
+
+        if user_choice == computer_guess:
+            computer_score = user_choice + computer_guess
+            print(f"The computer got {computer_score}")
+            comp_score = comp_score + computer_score
+            print(f"The computer's total is {comp_score}\n")
+        else:
+            print("The computer got 0")
+            print(f"The computer's total is {comp_score}\n")
