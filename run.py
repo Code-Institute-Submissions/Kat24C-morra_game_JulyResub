e134aa0a67c6e165c0b1c64241ca267fa2d44b98
Kat24C <kcsurvey47@gmail.com>
2022-07-10 12:30:38 +0000

    Fix incorrect wording

--- run.py
+++ run.py
@@ -32,28 +32,18 @@ def game_choice():
             user_choice = int(input("Choose a number between 1 and 3: "))
             while user_choice not in range(1, 4):
                 print("Please enter a number between 1 and 3")
-                user_choice = int(input("Choose a number between 1 and 3: "))     
+                user_choice = int(input("Choose a number between 1 and 3: "))
+            computer_guess = random.randint(1, 3)
+            print(f"The computer guessed: {computer_guess}")
             user_guess = int(input("Guess the computers number: "))
             while user_guess not in range(1, 4):
                 print("Please enter a number between 1 and 3")
                 user_guess = int(input("Guess the computers number: "))
+            computer_choice = random.randint(1, 3)
+            print(f"The computer chose: {computer_choice}\n")
         except ValueError:
             print("Please enter a valid number \n")
         else:
-            computer_guess = random.randint(1, 3)
-            print(f"The computer chose: {computer_guess}")
-            computer_choice = random.randint(1, 3)
-            print(f"The computer guessed: {computer_choice}\n")
-            if user_guess == computer_choice:
-                user_score = user_guess + computer_choice
-                print(f"Well done, You got {user_score}")
-                player_score = player_score + user_score
-                print(f"Your total is {player_score}\n")
-                finalgame(player_score, comp_score)
-            else:
-                print("Oh dear, try again you got 0")
-                print(f"Your total is {player_score}")
-                finalgame(player_score, comp_score)
             if user_choice == computer_guess:
                 computer_score = user_choice + computer_guess
                 print(f"The computer got {computer_score}")
@@ -63,6 +53,16 @@ def game_choice():
             else:
                 print("The computer got 0")
                 print(f"The computer's total is {comp_score}\n")
+                finalgame(player_score, comp_score)
+            if user_guess == computer_choice:
+                user_score = user_guess + computer_choice
+                print(f"Well done, You got {user_score}")
+                player_score = player_score + user_score
+                print(f"Your total is {player_score}\n")
+                finalgame(player_score, comp_score)
+            else:
+                print("Oh dear, try again you got 0")
+                print(f"Your total is {player_score}\n")
                 finalgame(player_score, comp_score)
 
 
